5d919361b79095e3fd94e8a8385f03d584e8a19f
datmaize <ngodat02012004@gmail.com>
2024-09-05 11:49:52 +0700

    finish order management page

--- src/pages/OrderManagerment/OrderManagermentPage.tsx
+++ src/pages/OrderManagerment/OrderManagermentPage.tsx
@@ -1,5 +1,21 @@
+import { Text } from '@/components/elements'
+import { OrderTable } from './OrderTable'
+
 const OrderManagermentPage = () => {
-	return <div>Order managerment</div>
+	return (
+		<div className='flex-1'>
+			<Text
+				variant={'secondary-bold'}
+				level={'h4'}
+				className='py-3 border-b border-gray-500'
+			>
+				Orders List
+			</Text>
+			<section className='overflow-x-auto p-0'>
+				<OrderTable />
+			</section>
+		</div>
+	)
 }
 
 export default OrderManagermentPage
